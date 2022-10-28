061acbfd097e8f1ad5dc0cbc8c9f73b0699053c7
Sven Glauser <75973240+SvenGlauser@users.noreply.github.com>
2022-10-28 16:47:21 +0200

    Création du module chat-application et création des Dto

--- chat-application/src/test/java/ch/sven/chat/application/utilisateur/dto/UtilisateurDtoTest.java
+++ chat-application/src/test/java/ch/sven/chat/application/utilisateur/dto/UtilisateurDtoTest.java
@@ -4,6 +4,7 @@ import ch.sven.chat.domain.utilisateur.model.Theme;
 import ch.sven.chat.domain.utilisateur.model.Utilisateur;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilisateurDtoTest {
@@ -12,6 +13,16 @@ class UtilisateurDtoTest {
     void utilisateurDto() {
         Utilisateur utilisateur = new Utilisateur();
         utilisateur.setTheme(Theme.SOMBRE);
+        utilisateur.setNom("Nom");
+        utilisateur.setPrenom("Prénom");
+        utilisateur.setImageUrl("https://google.com");
+
+        UtilisateurDto result = new UtilisateurDto(utilisateur);
+        assertThat(result).isNotNull();
+        assertThat(result.getNom()).isEqualTo(utilisateur.getNom());
+        assertThat(result.getPrenom()).isEqualTo(utilisateur.getPrenom());
+        assertThat(result.getTheme()).isEqualTo(utilisateur.getTheme().name());
+        assertThat(result.getImageUrl()).isEqualTo(utilisateur.getImageUrl());
     }
 
     @Test
